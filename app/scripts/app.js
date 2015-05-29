54fae6f5aa5bdf399c320b7064b4858a0a6be297
aaronw3373 <aaronw3373@gmail.com>
2015-05-28 21:43:30 -0400

    cleaned up code CRUD and Weather work next step is to seed some more data user auth etc

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,69 +1,17 @@
-$(document).ready(function() {
-
-  $('#login_button').on('click', function(){
-    console.log("loginging in");
-    var user = $('#usn').val()
-    var pass = $('#psw').val()
-    //run password checker against api
-    //if password checker successfull hide the login
-    // and display username
-    return false;
-  });
-
-  $('#search_button').on('click', function(){
-    console.log("searching");
-    //basic step one on submit search api for matches
-    //next level display resorts the match as you type
-    //and allow you to click or tab to the on you want
-    // like real search bars do
-    return false;
-  });
-
-  $('#about_button').on('click', function(){
-    console.log("about click");
-    //slide onto div row main a short about_page for the app and how/why
-    //make so clicking anything else hides the about_page
-  });
-
-  $('#resort_list').on('click', function(){
-    //step 1 reads my api
-    //displays info
-    //step 2 also reads weather info and displays info
-    //step 3 auto populates based on user location the closest resorts or favorites
-  });
-
-  $('#wunderground').on('click',function(){
-    window.open("http://www.wunderground.com/?apiref=390cac5ce90ab221");
-
-  });
-
-  $('#login_button').on('click', function(){
-    if ($('#usn').val() === "aaronw3373" && $('#psw').val() === "password1"){
-      console.log("loged in as super user");
-      $('#super_div').show()
-      $('#resort_info').hide()
-      $('#pow_factor').hide()
-      CRUDPRIV();
-    }
-  });
-
-});
-
-
-
+//The magic algorithm woah...
 function rating(vertical,acres,snowfall){
   return Math.round(((2000 + vertical + acres)/500)*(1+snowfall));
 };
 
-
-var needGo = "Call In Sick And Grab Your Boots";
-var betterGo = "I Doubt Your Boss Will Mind";
-var shouldGo = "It Better Be The Weekend Already";
-var maybeGo = "At Least Nobody Else Will Be There";
-var noGo = "If You Are Desperate But Then Again Spring Conditions";
-var summer = "Let's Go Surfing Instead"
-
+//determine which quote to display
 function quote(rating,month){
+  //variables for each quote as string
+  var needGo = "Call In Sick And Grab Your Boots";
+  var betterGo = "I Doubt Your Boss Will Mind";
+  var shouldGo = "It Better Be The Weekend Already";
+  var maybeGo = "At Least Nobody Else Will Be There";
+  var noGo = "If You Are Desperate But Then Again Spring Conditions";
+  var summer = "Let's Go Surfing Instead"
   if (month < 4 || month > 10){
     if ((rating/20) <= 1) {
       return noGo;
@@ -85,3 +33,134 @@ function quote(rating,month){
       return summer;
   }
 };
+
+//Allow CRUD privileges when logged in as super user
+//gives user these commands as click handlers
+function CRUDPRIV(){
+  $('#create_crud').on('click', function(){
+    createResortAjax(getCRUDData());
+    loadResortsAjax();
+  });
+  $('#read_crud_ID').on('click', function(){
+    c_json = getCRUDData()
+    var dbID = $('#ID_crud').val()
+    showResortAjax(dbID,"admin")
+  });
+  $('#read_crud_name').on('click', function(){
+    c_json = getCRUDData()
+    var searchAPI = resort_name + c_json.resort.name
+    searchAndShowResort(searchAPI,"admin")
+  });
+  $('#update_crud').on('click', function(){
+    var id = $('#ID_crud').val()
+    c_json = getCRUDData()
+    var updateAPI = resort_show + id
+    updateResortAjax(updateAPI,c_json);
+    loadResortsAjax();
+  });
+  $('#destroy_crud').on('click', function(){
+    c_json = getCRUDData()
+    var deleteAPI = resort_name + c_json.resort.name
+    deleteResortAjax(deleteAPI);
+    loadResortsAjax();
+  });
+}
+
+//retrieve CRUD data form info and return a json with it
+function getCRUDData(){
+  var c_name = $('#name_crud').val();
+  var c_vertical = $('#vertical_crud').val()
+  c_vertical = Number(c_vertical)
+  var c_acres = $('#acres_crud').val()
+  c_acres = Number(c_acres)
+  var c_state = $('#state_crud').val()
+  c_state = c_state.toUpperCase()
+  var c_location = $('#location_crud').val()
+  c_location = c_location.replace(/ /g,"_");
+  var c_json = {"resort":{"name":c_name,"vertical":c_vertical, "acres":c_acres,"state":c_state,"location":c_location}}
+  return c_json;
+}
+
+//
+//render AJAX responses
+//
+//render response for normal user
+function renderShowResort(data,weather){
+  var date = new Date();
+  $('#resort_name').html(data.name);
+  $('#vertical').html("Vertical Feet: " + data.vertical);
+  $('#acres').html("Skiable Acres: " + data.acres);
+  $('#rating').html("Powder Index: " + (rating(data.vertical,data.acres,0)));
+  $('#quote').html(quote(rating(data.vertical,data.acres),parseInt(date.getMonth()),0));
+  $('#current_temp_f').html("Temp: " + weather.current_observation.temp_f + " ℉");
+}
+
+//render response on superuser page
+function renderShowResortAdmin(data,weather){
+  $('#resorts_info_admin').html("<h3>"+ "ID: " +data.id+ " name: "+ data.name + " vertical: " + data.vertical +" acres: "+ data.acres + " state: " + data.state + " location: " + data.location + "</h3>");
+}
+
+//render response to resort list
+function appendResortList(resort){
+  $('#resort_list').append("<h3 class='resort' id=" + "resort" +resort.id + ">" + resort.name + "</h3>");
+}
+
+
+
+//
+//RUN TIME
+//
+//AND
+//
+//CLICK HANDLERS
+//
+
+
+$(document).ready(function() {
+  //poulate resort_list div
+  loadResortsAjax();
+
+  //resort list click to show resort
+  $('#resort_list').on('click', '.resort', function(event){
+    var dbID = parseInt(event.target.id.substring(6));
+    showResortAjax(dbID,"none");
+  });
+
+  //search bar on search_utton click
+  $('#search_button').on('click', function(){
+    console.log("search");
+    var search = $('#search_box').val()
+
+    var searchAPI = (resort_name + search)
+    searchAndShowResort(searchAPI,"none");
+    return false;
+  });
+
+  //about_button on click
+  $('#about_button').on('click', function(){
+    console.log("about click");
+    //slide onto div row main a short about_page for the app and how/why
+    //make so clicking anything else hides the about_page
+  });
+
+  //Click wunderground logo takes you to wunderground site
+  $('#wunderground').on('click',function(){
+    window.open("http://www.wunderground.com/?apiref=390cac5ce90ab221");
+
+  });
+
+  //login button on click with hard coded admin used info to CRUD
+  $('#login_button').on('click', function(){
+    if ($('#usn').val() === "aaronw3373" && $('#psw').val() === "password1"){
+      console.log("loged in as super user");
+      $('#super_div').toggle()
+      $('#resort_info').toggle()
+      $('#pow_factor').toggle()
+      CRUDPRIV();
+    }
+    return false;
+  });
+
+});
+
+
